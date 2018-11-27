5f1427098376e713eb2f72a88b8e3349605479c5
Jason Tu <jason_tu@trendmicro.com>
2018-11-26 22:16:34 -0500

    add fetchall code for ch1

---
+++ goProgrammingLanguage/ch1/fetchall.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+/*
+Test result:
+➜  ch1 git:(master) ✗ ./fetchall https://www.baidu.com https://google.com https://bing.com
+0.08s     227 https://www.baidu.com
+0.88s   13033 https://google.com
+5.91s  116929 https://bing.com
+5.91s elapsed
+ */
